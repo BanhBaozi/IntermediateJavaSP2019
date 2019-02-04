af6dd784f0f73efe9c51efe50ef13b34b2f74751
Jenny Nguyen <40232531+BanhBaozi@users.noreply.github.com>
2019-02-03 22:09:44 -0800

    Create Vehicle_Test.java

---
+++ Introduction/Week1 Lab1/Vehicle_Test.java
@@ -0,0 +1,9 @@
+public class Vehicle_Test {
+    //declaring main method in Vehicle_Test tab
+    public static void main(String[] args) {
+
+        VehicleManager Manage = new VehicleManager();
+        //calling run method of VehicleManager
+        Manage.run();
+    }
+}

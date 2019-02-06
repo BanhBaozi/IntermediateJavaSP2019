ae017d68e52b74f5539c3dc68f1ade9f91ff991b
Jenny Nguyen <40232531+BanhBaozi@users.noreply.github.com>
2019-02-05 20:43:51 -0800

    Update Util.java

--- Introduction/Week2/Util.java
+++ Introduction/Week2/Util.java
@@ -1,8 +1,10 @@
 //Imports required for hash function:
-
+import java.security.SecureRandom;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.util.ArrayList;
+
+import static java.lang.Thread.sleep;
 
 public class Util {
 
@@ -14,13 +16,9 @@ public class Util {
         MerkleNode oNode4 = new MerkleNode();
 
         //Filling in Hash values for leaf nodes
-
         oNode1.sHash = generateHash(lstItems.get(0));
-
         oNode2.sHash = generateHash(lstItems.get(1));
-
         oNode3.sHash = generateHash(lstItems.get(2));
-
         oNode4.sHash = generateHash(lstItems.get(3));
 
         MerkleNode oNode5 = new MerkleNode();
@@ -29,15 +27,15 @@ public class Util {
         oNode5.sHash = generateHash(oNode5.oLeft.sHash + oNode5.oRight.sHash);
 
         MerkleNode oNode6 = new MerkleNode();
-        oNode5.oLeft = oNode3;
-        oNode5.oRight = oNode4;
-        oNode5.sHash = generateHash(oNode6.oLeft.sHash + oNode6.oRight.sHash);
+        oNode6.oLeft = oNode3;
+        oNode6.oRight = oNode4;
+        oNode6.sHash = generateHash(oNode6.oLeft.sHash + oNode6.oRight.sHash);
 
         MerkleNode oNode7 = new MerkleNode();
-        oNode5.oLeft = oNode5;
-        oNode5.oRight = oNode6;
-        oNode5.sHash = generateHash(oNode7.oLeft.sHash + oNode7.oRight.sHash);
-        
+        oNode7.oLeft = oNode5;
+        oNode7.oRight = oNode6;
+        oNode7.sHash = generateHash(oNode7.oLeft.sHash + oNode7.oRight.sHash);
+
         return oNode7.sHash;
 
     }
@@ -60,6 +58,26 @@ public class Util {
 
             System.out.println("Error generating hash: " + ex.getMessage());
             return null;
+        }
+    }
+
+    public void sleepRandomTime(String sThreadName){
+
+        // Gets random number between 0 and 5 and then adds 3, meaning between 3 and 8 now.
+        int iSleepTime = new SecureRandom().nextInt(5) + 3;
+
+
+        //passes and prints the sleeping time
+        System.out.println(sThreadName + " sleeping for " + iSleepTime + " seconds.");
+        sleep(iSleepTime);
+    }
+
+    private void sleep(int iSeconds){
+        try{
+            Thread.sleep(iSeconds * 1000);
+        }
+        catch(Exception ex){
+
         }
     }
 }

107c6e380f1dc327038d3a60334bd8253b6b5c0f
Jenny Nguyen <40232531+BanhBaozi@users.noreply.github.com>
2019-05-21 00:22:18 -0700

    Update AreaGridTile.java

--- Introduction/Project 2/Agriculture Drone/com/dronerecon/ws/AreaGridTile.java
+++ Introduction/Project 2/Agriculture Drone/com/dronerecon/ws/AreaGridTile.java
@@ -1,11 +1,8 @@
 package com.dronerecon.ws;
 
 public class AreaGridTile {
-	public String areaID;
-	public int x;
-	public int y;
-	public int r;
-	public int g;
-	public String timestamp;
+    public String areaID;
+    public int x,r,y,g;
+    public String timestamp;
 
 }
